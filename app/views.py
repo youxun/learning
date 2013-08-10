735c46ae663cde5da98822a7dede3f2736aeee33
jerry_xing8@163 <jerry_xing8@163.com>
2013-08-10 14:31:37 +0800

    add the template content for flask such as extends,forend ...

--- app/views.py
+++ app/views.py
@@ -5,8 +5,17 @@ from flask import render_template
 @app.route('/index')
 def index
 	user = {'nickname':'Miguel'}
+	posts = [
+	{'author':{'nickname':'John'},
+	 'body':'Beautiful day in Portland!'
+	},
+	{'author':{'nickname':'Susan'},
+	 'body':'Think in java'
+	}
+	]
 	return render_template(
 	'index.html',
 	title = 'Home',
 	user = user,
+	posts = posts
 	)
