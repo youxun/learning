a0800a11c62a5a5fa4e8f282d6dc9d463c78d9d3
jerry_xing8@163 <jerry_xing8@163.com>
2013-08-13 14:29:10 +0800

    add the database create,migrate,upgrade,downgrade

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,19 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__) 
 app.config.from_object('config')
-from app import views
+db = SQLAlchemy(app)
+
+
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from app.config import basedir
+
+lm = LoginManager()
+lm.setup_app(app)
+oid = OpenID(app,os.path.join(basedir,'tmp'))
+lm.login_view = 'login'
+
+from app import models,views
--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,9 @@ from flask.ext.wtf import Required
 
 
 class LoginForm(Form):
-	opendid = TextField('openid',validators = [Required()])
+	openid = TextField('openid',validators = [Required()])
 	remember_me = BooleanField('remember_me',default = False)
+
+class EditForm(Form):
+	nickname = TextField('nickname',validators = [Required()])
+	about_me = TextField('about_me',validators =[Length(min =0,max = 140)])
